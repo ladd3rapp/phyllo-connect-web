febb6e1f04551c12022d9ff5e9b53aa2eff3bd03
Ladd3r <115385158+ladd3rapp@users.noreply.github.com>
2023-01-21 19:32:46 +0000

    api fixes

--- README.md
+++ README.md
@@ -6,10 +6,4 @@
 - Use NPM to install serve. Execute the command `npm install -g serve`.
 - Execute the command `serve` in the root folder of this project to start a HTTP server on your local machine to see this sample app running. 
 
-Note: Any simple HTTP server is fine to execute this. Using `serve` is not mandatory. 
-
-
-# Steps to Configure
-- Change the phyllo environment url in phylloAPIHelper.js via **PHYLLO_BASE_URL**
-- Enter your client_id in phylloAPIHelper.js line **PHYLLO_CLIENT_ID = "<CLIENT_ID>"**;
-- Enter your client_secret in phylloAPIHelper.js line **PHYLLO_SECRET_ID = "<CLIENT_SECRET>"**;
+;
--- dist/phylloAPIHelper.js
+++ dist/phylloAPIHelper.js
@@ -1,41 +1,50 @@
 
-// Base url For sandbox  
-const PHYLLO_BASE_URL = "https://api.sandbox.getphyllo.com";
+// Base url For staging  
+const XANO_BASE_URL = "https://x8ki-letl-twmt.n7.xano.io/api:1zR1S5hR";
 
-const URL_CREATE_SDK_TOKEN = "/v1/sdk-tokens";
-const URL_CREATE_USER = "/v1/users";
+// const URL_CREATE_SDK_TOKEN = "/v1/sdk-tokens";
+const URL_GET_ME = "/auth/me";
 
-const PHYLLO_CLIENT_ID = "null";
-const PHYLLO_SECRET_ID = "null";
+const url = new URL(window.location.toLocaleString());
+const parameters = new URLSearchParams(url.search);
+const auth = parameters.get('auth');
+const handle = parameters.get('handle');
+const external_id = parameters.get('external_id');
+const pid = parameters.get('pid');
+
+localStorage.setItem(external_id, pid, auth, handle); 
+sessionStorage.setItem(localStorage.getItem("auth"));
+console.log(localStorage.getItems("external_id","pid","auth","handle"));
 
 // encode client_id:secret to base-64
-const AUTH_KEY = window.btoa(PHYLLO_CLIENT_ID+":"+PHYLLO_SECRET_ID);
+const AUTH_KEY = auth;
 
 const getAxiosInstance = () => {
   const api = axios.create({
-    baseURL: PHYLLO_BASE_URL,
+    baseURL: XANO_BASE_URL,
     headers: {
-      'Authorization':'Basic ' + AUTH_KEY
+      'Authorization':'Bearer ' + AUTH_KEY
     },
   });
   return api;
+  console.log(api);
 };
 const createUser = async (name, externalId, isExistingUser) => {
   try {
-    const userId = await localStorage.getItem("PHYLLO_USERID");
+    const userId = await localStorage.getItem("id");
     if (isExistingUser) {
       return userId;
     } else if (!isExistingUser || !Boolean(userId)) {
       const api = getAxiosInstance();
-      let response = await api.post(URL_CREATE_USER, {
+      let response = await api.post(URL_GET_ME, {
         name: name,
         external_id: externalId,
       });
-      await localStorage.setItem("PHYLLO_USERID", response.data.id);
+      await localStorage.setItem("id", response.data.id);
       return response.data.id;
     }
   } catch (err) {
-    console.error(`Error ${err} occurred while generating user token`);
+    console.error(`Error ${err} occurred while retrieving the user`);
     return err.body;
   }
 };
@@ -47,9 +56,9 @@ const createSDKToken = async (userId) => {
   }
   try {
     const api = getAxiosInstance();
-    let response = await api.post(URL_CREATE_SDK_TOKEN, {
+    let response = await api.post(URL_GET_ME, {
       user_id: userId,
-      products: ["IDENTITY", "ENGAGEMENT"],
+      products: ["IDENTITY", "ENGAGEMENT", "IDENTITY.AUDIENCE", "ENGAGEMENT.AUDIENCE"],
     });
     return response.data.sdk_token;
   } catch (err) {
