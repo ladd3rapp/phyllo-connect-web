37422b41e3bb523aceedbbaa4107943f794de3a4
Ladd3r <115385158+ladd3rapp@users.noreply.github.com>
2023-03-13 18:26:19 -0700

    Update phylloAPIHelper.js

--- dist/phylloAPIHelper.js
+++ dist/phylloAPIHelper.js
@@ -2,23 +2,15 @@
 
 const url = new URL(window.location.toLocaleString());
 const parameters = new URLSearchParams(url.search);
-const handle = parameters.get('handle');
 const external_id = parameters.get('id');
 const pid = parameters.get('pid');
 const sdk = parameters.get('sdk');
-const isExistingUser = parameters.get('exists');
-const workpid = parameters.get('workpid');
-const platform = parameters.get('platform');
 
 
   localStorage.setItem('pid', pid.toString());
   localStorage.setItem('external_id', external_id.toString());
-  localStorage.setItem('handle', handle.toString());
   localStorage.setItem('sdk', sdk.toString());
   localStorage.setItem('isExistingUser', isExistingUser.toString());
-  localStorage.setItem('workpid', workpid.toString());
-  localStorage.setItem('platform', platform.toString());
-  
 
 
 
